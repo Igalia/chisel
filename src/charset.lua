c6fae18a7d48887eea68e7a361393f092cb3ec35
Adrian Perez <aperez@igalia.com>
2012-02-10 02:13:44 +0200

    Add charset module with ASCII constant codes

---
+++ src/charset.lua
@@ -0,0 +1,18 @@
+--
+-- charset.lua
+-- Copyright (C) 2012 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+local M = {
+	NUL = "\x00";
+	LF  = "\x0A";
+	FF  = "\x0C";
+	CR  = "\x0D";
+	ESC = "\x1B";
+	SPC = "\x20";
+}
+
+return M
+
