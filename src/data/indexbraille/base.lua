d0755c0a0251473506047d4ad4bf2d1486d9e305
Adrian Perez <aperez@igalia.com>
2012-02-14 05:20:15 +0200

    New "printerdata" module to define devices
    Information about devices is saved in Lua files, one file
    per device model. All devices inherit from "printerdata"
    and have support for generating (basic, for the moment)
    PPDs. The minimal data files for the Index Braille Basic-D
    are included.

---
+++ src/data/indexbraille/base.lua
@@ -0,0 +1,12 @@
+#! /usr/bin/env lua
+--
+-- base.lua
+-- Copyright (C) 2012 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+return lib.printerdata:extend {
+	manufacturer = "Index Braille";
+}
+
---
+++ src/data/indexbraille/basic-d.lua
@@ -0,0 +1,11 @@
+#! /usr/bin/env lua
+--
+-- basic-d.lua
+-- Copyright (C) 2012 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+return lib.printerdata:extend "indexbraille/base" {
+	model = "Basic-D";
+}
