2d538bd40d31cd8e90ea7d12b58c5adffdfc4b07
Adrian Perez <aperez@igalia.com>
2012-02-16 02:32:45 +0200

    Fix handling of non-flag command line options
    This fixes a by-1 offset error handling non-flag command line options (i.e.
    the ones passed to scripts). The bug caused chisel to segfault when not
    passing any flag in the command line.

--- src/chisel.c
+++ src/chisel.c
@@ -338,7 +338,7 @@ int
 main (int argc, char *argv[])
 {
     lua_State *L = NULL;
-    size_t consumed = 0;
+    size_t consumed = 1;
     int status;
 
     while ((status = getopt (argc, argv, "viS:L:h")) != -1) {
@@ -410,8 +410,8 @@ main (int argc, char *argv[])
      * lua_main above, which will add them to the Lua environment.
      */
     lua_pushcfunction (L, lua_main);
-    lua_pushinteger (L, argc - consumed + 1);
-    lua_pushlightuserdata (L, argv + consumed - 1);
+    lua_pushinteger (L, argc - consumed);
+    lua_pushlightuserdata (L, argv + consumed);
 
     if ((status = lua_pcall(L, 2, 0, 0)) != LUA_OK) {
         const char *msg = (lua_type (L, -1) == LUA_TSTRING) ? lua_tostring (L, -1)
