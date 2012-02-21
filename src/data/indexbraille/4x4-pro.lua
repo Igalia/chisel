bebcd618fea31bb48230d1f7d2948f2abb3c1056
Adrian Perez <aperez@igalia.com>
2012-02-21 22:08:15 +0200

    Add remaning Index Braille device data
    Adds the missing Index Braille devices:
    
    - 4Waves PRO
    - 4x4 PRO
    - Basic-S
    - Everest
    
    Note that only the IEEE1284 device identifier and the model name
    change. Some of the models have extra capabilities and those should
    be added later.

---
+++ src/data/indexbraille/4x4-pro.lua
@@ -0,0 +1,12 @@
+#! /usr/bin/env lua
+--
+-- basic-d.lua
+-- Copyright (C) 2012 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+base        = "indexbraille/base"
+model       = "4x4 PRO"
+ieee1284_id = "MFG:Index Braille AB; MDL:4x4 PRO;"
+
---
+++ src/data/indexbraille/basic-s.lua
@@ -0,0 +1,12 @@
+#! /usr/bin/env lua
+--
+-- basic-d.lua
+-- Copyright (C) 2012 Adrian Perez <aperez@igalia.com>
+--
+-- Distributed under terms of the MIT license.
+--
+
+base        = "indexbraille/base"
+model       = "Basic-S"
+ieee1284_id = "MFG:Index Braille AB; MDL:Basic-S;"
+
