5106aba3b1d7243382561d1164af0a27adbfe236
Adrian Perez <aperez@igalia.com>
2012-02-28 02:47:41 +0200

    Add media size information to indexbraille/_base
    Add the custom media sizes supported by the Index Braille devices, and the
    non-printable area margins to the "indexbraille/_base" data file.

--- src/data/indexbraille/_base.lua
+++ src/data/indexbraille/_base.lua
@@ -36,3 +36,20 @@ options = {
 		"NoTumble";
 	};
 }
+
+-- Printing areas and margins for the supported paper sizes
+-- Paper size does not need to be specified for standard ones.
+media = {
+	Letter = { margins={ 18; 36; 594;         756        } };
+	Legal  = { margins={ 18; 36; 594;         972        } };
+	A3     = { margins={ 18; 36; 823.889765; 1154.551180 } };
+	A4     = { margins={ 18; 36; 577.275590;  805.889765 } };
+	A5     = { margins={ 18; 36; 401.527560;  559.275590 } };
+
+	-- Non-standard paper sizes: specify both size and margins
+	["110x115"] = { size={ 792;  828 }; margins={ 18; 36; 774;  756 } };
+	["110x120"] = { size={ 782;  864 }; margins={ 18; 36; 774;  828 } };
+	["110x170"] = { size={ 792; 1224 }; margins={ 18; 36; 775; 1188 } };
+	["115x110"] = { size={ 828;  792 }; margins={ 18; 36; 756;  774 } };
+	["A4TF"   ] = { size={ 594;  846 }; margins={ 18; 36; 576;  810 } };
+}
